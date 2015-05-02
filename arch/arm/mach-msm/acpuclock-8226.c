befdea97935a4e2f6b31ed5ef9695b6a3b8c898d
vinay94185vinay <vinay94185vinay@yahoo.com>
2015-05-02 22:25:23 +0530

    added iops, overclocked slimbus and overclocked dtb

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -62,6 +62,7 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -73,6 +74,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -85,6 +87,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -100,6 +103,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
