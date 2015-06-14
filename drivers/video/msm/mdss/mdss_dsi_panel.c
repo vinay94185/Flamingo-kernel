01d86e26be8dcb19f016856d8bf30a5deb2ff7f1
faux123 <reioux@gmail.com>
2015-06-14 09:33:58 +0530

    mdss_dsi_panel.c: add powersuspend hooks Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,10 @@
 
 #include "mdss_dsi.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 //[All][Main][TP][DMS05342323][38605][thundertang] Reduce resume time(Resume performance) ++
@@ -509,6 +513,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -584,6 +592,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 #endif
 /*[Arima5908][32703][StevenChen] LCM driver porting 2014/01/03 end */
 /* [All][Main][LCM][DMS][StevenChen] Add 8926DS definition 2014/04/01 end */
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 	pr_debug("%s:-\n", __func__);
 	return 0;
