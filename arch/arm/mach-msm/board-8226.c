31cf31794ac797f262e38aa3b29df5b330367f1b
faux123 <reioux@gmail.com>
2015-06-14 20:38:29 +0530

    intelli-thermal: fix board 8226 merge error Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -127,8 +127,9 @@ void __init msm8226_add_drivers(void)
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
 #ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_device_init(NULL);
+	msm_thermal_init(NULL);
 #endif
+	msm_thermal_device_init(NULL);
 }
 
 void __init msm8226_init(void)
